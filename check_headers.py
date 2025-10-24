e306cc5ba38b5c16f13ca400ac70a74cc633699c
Benjamin Boehm <boehmbt@protonmail.com>
2025-10-24 08:10:23 +0200

    Repo cleanup: entferne alte Dateien und große Datenmengen

--- check_headers.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-import os
-
-# Pfad zum ersten CSV-Datensatz
-file = os.path.join("data", os.listdir("data")[0])  # Passe "data" ggf. an
-
-df = pd.read_csv(file)
-print("Spaltenüberschriften:", df.columns.tolist())
